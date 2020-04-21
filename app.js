35310816e64c22fd2faf6d677bcc8cc81af5a157
Chhechinyong <chhechinyong01@gmail.com>
2020-04-21 17:48:19 +0700

    testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app=express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const path =
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
