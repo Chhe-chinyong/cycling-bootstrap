ce12d685b43c0daec6fc8822061347fff9a365c2
Chhechinyong <chhechinyong01@gmail.com>
2020-05-04 23:04:00 +0700

    update routes

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app=express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path =
+const path = require("path");
+
+const public = path.join(__dirname,"/public");
+
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(public));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// app.get('/',(req,res)=>{
-//     res.render('/Users/chinyongchhe/Documents/Project/hello-world/public/index.html')
-// })
+app.get('/', (req,res) => {
+    res.sendFile(public+"/index.html");
+})
+
+app.get("/register", (req,res) =>{
+    res.sendFile(public + "/register.html");
+})
+
 var port = Number(process.env.PORT || 3000);
 app.listen(port,'0.0.0.0',(req, res) => {
     console.log('running');
