0086adfb28c6bf5d4051f2eb55ae2959c9828f4a
Chhechinyong <chhechinyong01@gmail.com>
2020-05-06 00:34:05 +0700

    Update-container

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req,res) => {
-    res.sendFile(public+"/index.html");
+    res.status(200).sendFile(public+"/index.html");
 })
 
 app.get("/register", (req,res) =>{
-    res.sendFile(public + "/register.html");
+    res.status(200).sendFile(public + "/register.html");
 })
 
+
+app.get("*", (req,res) => {
+    res.status(404).sendFile(public + "/error.html");
+})
 var port = Number(process.env.PORT || 3000);
 app.listen(port,'0.0.0.0',(req, res) => {
     console.log('running');
